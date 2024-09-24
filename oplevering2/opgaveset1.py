d83d736ac6cff7bb0994be7ad3143c675274acaf
Matthias <m.l.nijman@st.hanze.nl>
2024-09-24 15:40:05 +0200

    start opgave 1

--- oplevering2/opgaveset1.py
+++ oplevering2/opgaveset1.py
@@ -1,3 +1,15 @@
-import sklearn
+from sklearn import datasets
+import numpy as np
 
-sklearn.datasets.load_iris
+# Import dataset load_iris, put data into X and target into y
+X, y = datasets.load_iris(return_X_y=True)
+
+# Set every 1 to 0 and every 2 to 1
+for i in range(len(y)):
+    if y[i] == 1:
+        y[i] = 0
+    if y[i] == 2:
+        y[i] = 1
+
+def sigmoid(theta):
+    ...
